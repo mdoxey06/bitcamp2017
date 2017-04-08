418febc60111abdbf4ec4e812284983bba9f3886
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 15:03:31 -0400

    spotify redirect

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/webhook/', function (req, res) {
 		res.send(req.query['hub.challenge'])
 	}
 	if (req.query['qs.access_token'] === "token") {
-		res.send("success!")
+		res.send("success webhook!")
 	}
 	res.send("Error, wrong token")
 });
@@ -49,62 +49,62 @@ app.get('/callback/', function(req, res) {
 
   // your application requests refresh and access tokens
   // after checking the state parameter
+  res.send("success callback!")
+  // var code = req.query.code || null;
+  // var state = req.query.state || null;
+  // var storedState = req.cookies ? req.cookies[stateKey] : null;
 
-  var code = req.query.code || null;
-  var state = req.query.state || null;
-  var storedState = req.cookies ? req.cookies[stateKey] : null;
+  // if (state === null || state !== storedState) {
+  //   res.redirect('/#' +
+  //     querystring.stringify({
+  //       error: 'state_mismatch'
+  //     }));
+  // } else {
+  //   res.clearCookie(stateKey);
+  //   var authOptions = {
+  //     url: 'https://accounts.spotify.com/api/token',
+  //     form: {
+  //       code: code,
+  //       redirect_uri: redirect_uri,
+  //       grant_type: 'authorization_code'
+  //     },
+  //     headers: {
+  //       'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+  //     },
+  //     json: true
+  //   };
 
-  if (state === null || state !== storedState) {
-    res.redirect('/#' +
-      querystring.stringify({
-        error: 'state_mismatch'
-      }));
-  } else {
-    res.clearCookie(stateKey);
-    var authOptions = {
-      url: 'https://accounts.spotify.com/api/token',
-      form: {
-        code: code,
-        redirect_uri: redirect_uri,
-        grant_type: 'authorization_code'
-      },
-      headers: {
-        'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
-      },
-      json: true
-    };
+  //   request.post(authOptions, function(error, response, body) {
+  //     if (!error && response.statusCode === 200) {
 
-    request.post(authOptions, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
+  //       var access_token = body.access_token,
+  //           refresh_token = body.refresh_token;
 
-        var access_token = body.access_token,
-            refresh_token = body.refresh_token;
+  //       var options = {
+  //         url: 'https://api.spotify.com/v1/me',
+  //         headers: { 'Authorization': 'Bearer ' + access_token },
+  //         json: true
+  //       };
 
-        var options = {
-          url: 'https://api.spotify.com/v1/me',
-          headers: { 'Authorization': 'Bearer ' + access_token },
-          json: true
-        };
+  //       // use the access token to access the Spotify Web API
+  //       request.get(options, function(error, response, body) {
+  //         console.log(body);
+  //       });
 
-        // use the access token to access the Spotify Web API
-        request.get(options, function(error, response, body) {
-          console.log(body);
-        });
-
-        // we can also pass the token to the browser to make requests from there
-        res.redirect('/#' +
-          querystring.stringify({
-            access_token: access_token,
-            refresh_token: refresh_token
-          }));
-      } else {
-        res.redirect('/#' +
-          querystring.stringify({
-            error: 'invalid_token'
-          }));
-      }
-    });
-  }
+  //       // we can also pass the token to the browser to make requests from there
+  //       res.redirect('/#' +
+  //         querystring.stringify({
+  //           access_token: access_token,
+  //           refresh_token: refresh_token
+  //         }));
+  //     } else {
+  //       res.redirect('/#' +
+  //         querystring.stringify({
+  //           error: 'invalid_token'
+  //         }));
+  //     }
+  //   });
+  // }
 });
 
 app.post('/webhook/', function (req, res) {
