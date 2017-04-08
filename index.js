e563f52529df61421edcb72b30606694c5a70da5
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 03:29:47 -0400

    updated bot to respond

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const request = require('request')
 const app = express()
 
 app.set('port', (process.env.PORT || 5000))
+app.use(bodyParser.json())
 
 // Process application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: false}))
