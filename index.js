55d40eaceeebf1d5236dceb3f84a05ced3071751
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 15:09:45 -0400

    spotify redirect

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.get('/callback/', function(req, res) {
         request.get(options, function(error, response, body) {
           console.log(body);
         });
+    }
 
   //       // we can also pass the token to the browser to make requests from there
   //       res.redirect('/#' +
