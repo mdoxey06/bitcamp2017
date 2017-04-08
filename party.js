ce2945e17b2a15c98dcffde3b107254969f91324
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 18:55:26 -0400

    added party class

--- party.js
+++ party.js
@@ -1,5 +1,41 @@
+const Guest = require('./guest.js')
+const Request = require('./request.js')
+
 var method = Party.prototype;
 
-function Party() {
-	
+function Party(partyName, pw, hostID, playlist) {
+	this._partyName = partyName;
+	this._password = pw;
+	this._hostID = hostID;
+	this._playlist = playlist;
+	this._requests = [];
+	this._guests = [host]
 }
+
+method.getPartyName = function() {
+	return partyName;
+};
+
+method.getPlaylistURI = function() {
+	return playlist;
+};
+
+method.getGuests = function() {
+	var guestIDs = [];
+	this._guests.foreach(function(guest) {
+		guestIDs.push(guest.getSenderID);
+	});
+	return guestIDs;
+};
+
+method.addGuest = function(newGuest) {
+	this._guests.push(newGuest);
+};
+
+method.addRequest = function(newRequest) {
+	this._requests.push(newRequest);
+};
+
+method.getFirstRequest = function() {
+	return this._requests.shift();
+};
