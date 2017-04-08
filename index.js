e7806f67e1305c8eadae33a0519a45bedaa70709
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 14:54:27 -0400

    spotify redirect

--- index.js
+++ index.js
@@ -39,13 +39,13 @@ function dump(obj) {
         out += i + ": " + obj[i] + "\n";
     }
 
-    alert(out);
+    console.log(out);
 
     // or, if you wanted to avoid alerts...
 
-    var pre = document.createElement('pre');
-    pre.innerHTML = out;
-    document.body.appendChild(pre)
+    // var pre = document.createElement('pre');
+    // pre.innerHTML = out;
+    // document.body.appendChild(pre)
 }
 
 // for Facebook verification
