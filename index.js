1d7bdbd441b2c983f6fa1579aaa561f3da771ad8
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 02:10:55 -0400

    updated bot to speak

--- index.js
+++ index.js
@@ -30,3 +30,49 @@ app.get('/webhook/', function (req, res) {
 app.listen(app.get('port'), function() {
 	console.log('running on port', app.get('port'))
 })
+
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+	    if (event.message && event.message.text) {
+		    let text = event.message.text
+		    sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+	    }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAStcXwYvj0BADPAa2AVETYZCQvaU1MPWWEeZBCocXcOF0vtoGNxkam2E4ZCNkvKZCGL6lDz7yuZAjgObDTtIhu9WViV3chfCVRdSZA4wr1W4vrXYuUxMzsnopZCmHmNcR5h7omYr43W28v1gON7h1vS20eAga2c7vGFnNbTLSyfAZDZD"
+
+
+function sendTextMessage(sender, text) {
+    let messageData = { text:text }
+    request({
+	    url: 'https://graph.facebook.com/v2.6/me/messages',
+	    qs: {access_token:token},
+	    method: 'POST',
+		json: {
+		    recipient: {id:sender},
+			message: messageData,
+		}
+	}, function(error, response, body) {
+		if (error) {
+		    console.log('Error sending messages: ', error)
+		} else if (response.body.error) {
+		    console.log('Error: ', response.body.error)
+	    }
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
