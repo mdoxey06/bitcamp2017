b1992a9674b22c698f0d8d049520a43c10e73389
mdoxey06 <mdoxey06@gmail.com>
2017-04-08 22:32:11 -0400

    createParty

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const request = require('request')
+const passport = require('passport')
 const app = express()
 var querystring = require('qs');
 var Party = require("./party.js")
@@ -82,6 +83,11 @@ app.get('/callback/', function(req, res) {
 
 	res.redirect("https://www.messenger.com/t/414205672270256");
 });
+
+app.get('/auth/spotify', passport.authenticate('spotify', 
+	{scope: ['user-read-private', 'user-read-email', 'playlist-read-private', 'playlist-modify-private', 'streaming'], showDialog: true}),
+	function(req, res) {
+	});
 
 var createPartyRE = /^createparty \"(.+)\" \"(.+)\"$/
 var joinParty = /^joinparty \"(.+)\" \"(.+)\"$/
@@ -200,55 +206,6 @@ function sendTextMessage(sender, text) {
 		if (error) {
 		    console.log('Error sending messages: ', error)
 		} else if (response.body.error) {
-		    console.log('Error: ', response.body.error)
-	    }
-    })
-}
-
-function sendGenericMessage(sender) {
-    let messageData = {
-	    "attachment": {
-		    "type": "template",
-		    "payload": {
-				"template_type": "generic",
-			    "elements": [{
-					"title": "First card",
-				    "subtitle": "Element #1 of an hscroll",
-				    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-				    "buttons": [{
-					    "type": "web_url",
-					    "url": "https://www.messenger.com",
-					    "title": "web url"
-				    }, {
-					    "type": "postback",
-					    "title": "Postback",
-					    "payload": "Payload for first element in a generic bubble",
-				    }],
-			    }, {
-				    "title": "Second card",
-				    "subtitle": "Element #2 of an hscroll",
-				    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-				    "buttons": [{
-					    "type": "postback",
-					    "title": "Postback",
-					    "payload": "Payload for second element in a generic bubble",
-				    }],
-			    }]
-		    }
-	    }
-    }
-    request({
-	    url: 'https://graph.facebook.com/v2.6/me/messages',
-	    qs: {access_token:token},
-	    method: 'POST',
-	    json: {
-		    recipient: {id:sender},
-		    message: messageData,
-	    }
-    }, function(error, response, body) {
-	    if (error) {
-		    console.log('Error sending messages: ', error)
-	    } else if (response.body.error) {
 		    console.log('Error: ', response.body.error)
 	    }
     })
